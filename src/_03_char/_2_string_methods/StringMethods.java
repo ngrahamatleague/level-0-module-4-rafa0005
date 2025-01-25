a36a8b8d4622df778e45ab939c2b52b9c4b78602
RafaGobran <160378515+RafaGobran@users.noreply.github.com>
2025-01-25 12:53:50 -0800

    today i did charAt, and length

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -5,26 +5,37 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+			String name = "Rafa";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-
+			System.out.println(name.charAt(2));
+			
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
-		
+			System.out.println(name.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
-		
+			for(int i = 0; i < name.length();i++) {
+				System.out.println(name.charAt(i));
+			}
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
-	}
+			for(int i = 0; i < name.length();i++) {
+			
+			 char c = name.charAt(i);
+			 if(c == 'f') {
+				 System.out.println("f was found at index " + i);
+			 }
+			 
+	
+			}
+			
+	
+}
 }
 
 
--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -2,6 +2,8 @@ package _03_char._3_pi_aloud;
 
 import java.util.Scanner;
 
+import game_tools.Sound;
+
 public class PiAloud {
     /***********  SOUND ***************
      * Some computers are unable to play sounds.
@@ -12,14 +14,22 @@ public class PiAloud {
 
 
     // 1. Make a main method and make sure your program can run
+    public static void main(String[] args) {
+	
 
     // 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
-
+  String pi = "3.1415926535897932384";
     // 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
     // Check the console shows 3.1
-
+  	
+  System.out.print(pi.charAt(0));
+  System.out.print(pi.charAt(1));
+  System.out.println(pi.charAt(2));
     // 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
-
+  		for(int i = 0; i < pi.length();i++) {
+  			 System.out.println(pi.charAt(i));
+  			 Sound.speak(String.valueOf(pi.charAt(i)));
+  		}
     // 5. Skip this step if your computer cannot play sounds.
     //if (canPlaySounds) {
         // Use the Sound.speak() method to speak each digit of Pi.
@@ -32,7 +42,7 @@ public class PiAloud {
     // *7. Compare the users' char to the next digit of Pi
     // *8. If they are correct, print out "correct". If they are not, print "incorrect"
 
-
+}
     /********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 
     private static void pause(int seconds) {
@@ -49,5 +59,7 @@ public class PiAloud {
     }
 
 }
+
+
 
 
